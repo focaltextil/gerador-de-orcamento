d3911dae4fd5d025d57464c6951c64194d1d3c93
Focal-textil <focalti@gmail.com>
2025-01-28 14:43:21 -0300

    updated

--- scripts/pedido.js
+++ scripts/pedido.js
@@ -67,7 +67,7 @@ loadExcelData()
 // FAZER DROPDOWN
 
 function populateDropdown() {
-    // Recupera os dados do sessionStorage
+ 
     const savedData = sessionStorage.getItem('excelData');
     
     if (!savedData) {
@@ -75,37 +75,213 @@ function populateDropdown() {
         return;
     }
 
-    // Converte os dados para JSON
+
     const excelData = JSON.parse(savedData);
 
-    // Verifica se os dados têm a estrutura correta
+
     if (excelData.length === 0) {
         console.error('Os dados estão vazios!');
         return;
     }
 
-    // Pega o dropdown
+
     const dropdown = document.getElementById('filterSelect1');
 
-    // Pega a primeira coluna (Categoria) dos dados
-    const uniqueCategories = [...new Set(excelData.map(item => item.Descrição))];
 
-    // Verifica se encontramos categorias únicas
+    const uniqueCategories = [...new Set(excelData.filter(item => item.Tipo === "DIVERSOS").map(item => item.Descrição))];
+    
+    
+    // ----------------------------------------------------------------------------
+
+    const dropdown2 = document.getElementById('filterSelect2');
+
+    const uniqueCategories2 = [...new Set(excelData.filter(item => item.Tipo === "DUBLADOS PROMOCIONAIS").map(item => item.Descrição))];
+    
+    
+    // ----------------------------------------------------------------------------
+
+    const dropdown3 = document.getElementById('filterSelect3');
+
+    const uniqueCategories3 = [...new Set(excelData.filter(item => item.Tipo === "DUBLAGEM").map(item => item.Descrição))];
+    
+    
+    // ----------------------------------------------------------------------------
+
+        
+    const dropdown4 = document.getElementById('filterSelect4');
+
+    const uniqueCategories4 = [...new Set(excelData.filter(item => item.Tipo === "ESPUMAS").map(item => item.Descrição))];
+    
+    
+    // ----------------------------------------------------------------------------
+
+            
+    const dropdown5 = document.getElementById('filterSelect5');
+
+    const uniqueCategories5 = [...new Set(excelData.filter(item => item.Tipo === "ESTAMPARIA").map(item => item.Descrição))];
+    
+    
+    // ----------------------------------------------------------------------------
+
+                
+    const dropdown6 = document.getElementById('filterSelect6');
+
+    const uniqueCategories6 = [...new Set(excelData.filter(item => item.Tipo === "EVA").map(item => item.Descrição))];
+    
+    
+    // ----------------------------------------------------------------------------
+
+    const dropdown7 = document.getElementById('filterSelect7');
+
+    const uniqueCategories7 = [...new Set(excelData.filter(item => item.Tipo === "MALHAS ALGODÃO").map(item => item.Descrição))];
+    
+    
+    // ----------------------------------------------------------------------------
+
+    const dropdown8 = document.getElementById('filterSelect8');
+
+    const uniqueCategories8 = [...new Set(excelData.filter(item => item.Tipo === "MALHAS POLIESTER/ ALGODÃO").map(item => item.Descrição))];
+    
+    
+    // ----------------------------------------------------------------------------
+
+    const dropdown9 = document.getElementById('filterSelect9');
+
+    const uniqueCategories9 = [...new Set(excelData.filter(item => item.Tipo === "METALASSÊ").map(item => item.Descrição))];
+    
+    
+    // ----------------------------------------------------------------------------
+
+    const dropdown10 = document.getElementById('filterSelect10');
+
+    const uniqueCategories10 = [...new Set(excelData.filter(item => item.Tipo === "NÃO TECIDOS").map(item => item.Descrição))];
+    
+    
+    // ----------------------------------------------------------------------------
+
+    const dropdown11 = document.getElementById('filterSelect11');
+
+    const uniqueCategories11 = [...new Set(excelData.filter(item => item.Tipo === "TECIDOS").map(item => item.Descrição))];
+    
+    
+    // ----------------------------------------------------------------------------
+
+    const dropdown12 = document.getElementById('filterSelect12');
+
+    const uniqueCategories12 = [...new Set(excelData.filter(item => item.Tipo === "TECIDOS MALHAS POR KILO (ESTADO SP)").map(item => item.Descrição))];
+    
+    
+    // ----------------------------------------------------------------------------
+
     if (uniqueCategories.length === 0) {
         console.error('Não foram encontradas categorias!');
         return;
     }
 
-    // Adiciona as opções no dropdown
     uniqueCategories.forEach(category => {
         const option = document.createElement('option');
         option.value = category;
         option.textContent = category;
         dropdown.appendChild(option);
     });
+
+    uniqueCategories2.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category;
+        option.textContent = category;
+        dropdown2.appendChild(option);
+    });
+
+    uniqueCategories3.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category;
+        option.textContent = category;
+        dropdown3.appendChild(option);
+    });
+
+    uniqueCategories4.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category;
+        option.textContent = category;
+        dropdown4.appendChild(option);
+    });
+
+    uniqueCategories5.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category;
+        option.textContent = category;
+        dropdown5.appendChild(option);
+    });
+
+    uniqueCategories6.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category;
+        option.textContent = category;
+        dropdown6.appendChild(option);
+    });
+
+    uniqueCategories7.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category;
+        option.textContent = category;
+        dropdown7.appendChild(option);
+    });
+
+    uniqueCategories8.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category;
+        option.textContent = category;
+        dropdown8.appendChild(option);
+    });
+
+    uniqueCategories9.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category;
+        option.textContent = category;
+        dropdown9.appendChild(option);
+    });
+
+    uniqueCategories10.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category;
+        option.textContent = category;
+        dropdown10.appendChild(option);
+    });
+
+    uniqueCategories11.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category;
+        option.textContent = category;
+        dropdown11.appendChild(option);
+    });
+
+    uniqueCategories12.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category;
+        option.textContent = category;
+        dropdown12.appendChild(option);
+    });
 }
 
-// Chama a função para preencher o dropdown assim que a página for carregada
+// ----------------------------------------------------------------------------
+//FUNCAO DEIVIDIANA PARA PEGAR O VALOR DO DROPDOWN
+
+document.addEventListener("DOMContentLoaded", function () {
+    const botoes = document.querySelectorAll(".add-btn");
+
+    botoes.forEach(botao => {
+        botao.addEventListener("click", function () {
+            const selectId = this.getAttribute("data-target");
+            const select = document.getElementById(selectId);
+            console.log(select.value);
+        });
+    });
+});
+
+
+// ----------------------------------------------------------------------------
+
+
 window.onload = function() {
     populateDropdown();
 };
